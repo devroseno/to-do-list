660e9d45b161a8e30ce2a28c0b05703413ffdc88
eudanielroseno <contatodanielroseno@gmail.com>
2022-09-27 14:04:11 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
+import { useState } from 'react';
+import * as C from './App.styles';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  const [list, setList] = useState([]);
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+      </C.Area>
+    </C.Container>
   );
 }
 
